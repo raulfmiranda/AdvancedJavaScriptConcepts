7e5dead94ef4f258bcc7d39433c3eae3d594888d
Raul Miranda <raul.miranda@osf-commerce.com>
2019-11-12 14:30:59 -0300

    Course is done.

---
+++ 13-183 firstRecurringCharacter.js
@@ -0,0 +1,32 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+    if (input instanceof Array && input.length > 1) {
+        let arr = input.slice();
+        while(arr.length > 0) {
+            let item = arr.shift();
+            if (arr.includes(item)) {
+                return item;
+            }
+        }
+    }
+    return undefined;
+}
+
+console.log(
+    firstRecurringCharacter([2,5,1,2,3,5,1,2,4]),
+    firstRecurringCharacter([2,1,1,2,3,5,1,2,4]), 
+    firstRecurringCharacter([2,3,4,5])
+);
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
